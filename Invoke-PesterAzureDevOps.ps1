6559d769efa94a7dc8b1ee80f8f2740a26072792
sumitdubeyit <34869158+sumitdubeyit@users.noreply.github.com>
2020-01-15 00:09:18 +0530

    Update Invoke-PesterAzureDevOps.ps1

--- Invoke-PesterAzureDevOps.ps1
+++ Invoke-PesterAzureDevOps.ps1
@@ -1,4 +1,3 @@
- Install-Module az.accounts -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name Pester -Force -SkipPublisherCheck
 Import-Module Pester
 $outputfile=".\Test-Results.xml"
