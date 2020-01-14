9e6cae63df426adcc3803705c4c7c1114554e010
sumitdubeyit <34869158+sumitdubeyit@users.noreply.github.com>
2020-01-15 00:34:32 +0530

    Update azure-vm.steps.ps1

--- azure-vm.steps.ps1
+++ azure-vm.steps.ps1
@@ -1,3 +1,5 @@
+Install-Module -Name Pester -Force -SkipPublisherCheck
+Import-Module Pester
 Given "the deployment is successful" {}
 Then "the RG name should be SQL" {
 Get-AzResourceGroup |?{$_.ResourceGroupName -eq "SQL"}|select ResourceGroupName -ExpandProperty ResourceGroupName |Should -Be "SQL" 
