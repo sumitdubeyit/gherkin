f128a0c30148abbcb595bb8e2ffa5bd33b73d68f
sumitdubeyit <34869158+sumitdubeyit@users.noreply.github.com>
2020-01-15 16:10:21 +0530

    Update azure.ps1

--- devops/azure.ps1
+++ devops/azure.ps1
@@ -1,5 +1,5 @@
 Install-Module -Name Pester -Force -SkipPublisherCheck
 Import-Module Pester
 Invoke-Gherkin -path "D:\a\1\s\devops" Test-Pester1.xml -OutputFormat NUnitXml
-gci $(System.DefaultWorkingDirectory)
+
 
