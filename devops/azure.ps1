60768b9c32d579883ba5890f37278064f277605d
sumitdubeyit <34869158+sumitdubeyit@users.noreply.github.com>
2020-01-16 17:46:43 +0530

    Update azure.ps1

--- devops/azure.ps1
+++ devops/azure.ps1
@@ -1,5 +1,5 @@
 Install-Module -Name Pester -Force -SkipPublisherCheck
 Import-Module Pester
-Invoke-Gherkin -path "./devops" -OutputFile "./TEST-Pester-1.xml" -OutputFormat NUnitXml
+Invoke-Gherkin 
 
 
